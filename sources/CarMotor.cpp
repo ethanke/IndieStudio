6390ca04dce22f07a8cdd099b98d9fc7d3f0bed4
victor sousa <victor.sousa@epitech.eu>
2017-05-14 00:46:51 +0200

    compile v2

--- sources/CarMotor.cpp
+++ sources/CarMotor.cpp
@@ -5,7 +5,7 @@
 // Login   <sousa_v@epitech.eu>
 //
 // Started on  Sat May 13 22:34:06 2017 Sousa Victor
-// Last update Sun May 14 00:30:16 2017 Sousa Victor
+// Last update Sun May 14 00:45:59 2017 Sousa Victor
 //
 
 #include "CarMotor.hpp"
@@ -15,6 +15,8 @@ using namespace indie;
 CarMotor::CarMotor(irr::gui::IGUIEnvironment* guiManager, bool debugMode) {
     this->_gui = guiManager;
     this->_debug = debugMode;
+
+     this->_inputGui = this->_gui->addStaticText(L"intput,  X: 0,  Y: 0", irr::core::rect<irr::s32>(0, 0, 500, 100));
 }
 
 CarMotor::~CarMotor() {
