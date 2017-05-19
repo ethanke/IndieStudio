38919688b0bb9c7169546864dea2cc58baf75a48
victor sousa <victor.sousa@epitech.eu>
2017-05-19 21:33:45 +0200

    - missing files

--- sources/CarSystem/Car.cpp
+++ sources/CarSystem/Car.cpp
@@ -5,7 +5,7 @@
 // Login   <sousa_v@epitech.eu>
 //
 // Started on  Thu May 11 23:18:00 2017 Sousa Victor
-// Last update Fri May 19 21:22:14 2017 Sousa Victor
+// Last update Fri May 19 21:33:21 2017 Sousa Victor
 //
 
 #include "Car.hpp"
@@ -18,7 +18,7 @@ Car::Car(irr::scene::ISceneManager *sceneManager, irr::gui::IGUIEnvironment* gui
     this->_eventReceiver = eventReceiver;
     this->_camera = new BasicCamera(this->_smgr, 0, -1, irr::core::vector3df(10, 37.5, 4), irr::core::vector3df(2, 36, 0));
 
-    this->_carNode = this->_smgr->addMeshSceneNode(this->_smgr->getMesh("/Users/vicostudio/Documents/Shared/TEK2/CPP/UniversityRacer/models/car.3ds"));
+    this->_carNode = this->_smgr->addMeshSceneNode(this->_smgr->getMesh("car/Car/SV.obj"));
     this->_carNode->setMaterialFlag(irr::video::EMF_LIGHTING, false);
     this->_carNode->setPosition(irr::core::vector3df(2, 38, 4));
 
