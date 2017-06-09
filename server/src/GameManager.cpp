244a4c3d5321bd78fe0e19ef6f4f10d243258b95
Gambini Lucas <lucas.gambini@epitech.eu>
2017-06-10 01:04:34 +0200

    Ok mon dernier commit ne marchait pas a > 2 clients dans la meme instance pour une connerie, c'est fix

--- server/src/GameManager.cpp
+++ server/src/GameManager.cpp
@@ -61,16 +61,17 @@ void GameManager::deleteClient(int fd)
     if (this->_clients.count(fd) > 0)
     {
         std::cout << "Disconnection : " << this->getClientByFd(fd).getSocket().getIp() << std::endl;
-        this->_clients.erase(fd);
         for (auto &server : this->_servers) {
             if (server.foundClientByFd(fd)) {
                 server.deleteClientByFd(fd);
-                if (server.getClientNumber() == 1)
+                if (server.getClientNumber() == 1) {
                     server.shutdown();
                     this->_servers.pop_back();
-                    break;
+                }
+                break;
             }
         }
+        this->_clients.erase(fd);
     }
 }
 
