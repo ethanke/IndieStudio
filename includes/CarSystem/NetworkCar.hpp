17163652b7afd00813d40fa05c1b914848812375
victor sousa <victor.sousa@epitech.eu>
2017-06-10 16:43:15 +0200

    net car class, network class, doing nothing, you can just set it's velocity and position

---
+++ includes/CarSystem/NetworkCar.hpp
@@ -0,0 +1,30 @@
+//
+// NetworkCar.hpp for  in /Users/vicostudio/Documents/Shared/TEK2/CPP/IndieStudio/includes/CarSystem/
+//
+// Made by Sousa Victor
+// Login   <sousa_v@epitech.eu>
+//
+// Started on  Sat Jun 10 16:38:37 2017 Sousa Victor
+// Last update Sat Jun 10 16:41:46 2017 Sousa Victor
+//
+
+#ifndef NETWORKCAR_HPP_
+#define NETWORKCAR_HPP_
+
+#include "Car.hpp"
+
+namespace indie {
+
+    class NetworkCar : public Car {
+
+    public:
+        NetworkCar(irr::scene::ISceneManager *sceneManager, irr::gui::IGUIEnvironment* guiManager, EventReceiver *eventReceiver, physics::CBulletPhysics *bulletPhysicsSystem, Circuit const &circuit, int car_no = 0);
+        ~NetworkCar();
+
+        virtual void OnFrame();
+
+    };
+
+}
+
+#endif /*NETWORKCAR_HPP_*/
---
+++ sources/CarSystem/NetworkCar.cpp
@@ -0,0 +1,26 @@
+//
+// NetworkCar.cpp for  in /Users/vicostudio/Documents/Shared/TEK2/CPP/IndieStudio/sources/CarSystem/
+//
+// Made by Sousa Victor
+// Login   <sousa_v@epitech.eu>
+//
+// Started on  Sat Jun 10 16:38:11 2017 Sousa Victor
+// Last update Sat Jun 10 16:41:03 2017 Sousa Victor
+//
+
+#include "NetworkCar.hpp"
+
+using namespace indie;
+
+NetworkCar::NetworkCar(irr::scene::ISceneManager *sceneManager, irr::gui::IGUIEnvironment* guiManager, EventReceiver *eventReceiver, physics::CBulletPhysics *bulletPhysicsSystem, Circuit const &circuit, int car_no):
+    Car(sceneManager, guiManager, eventReceiver, bulletPhysicsSystem, circuit, car_no, irr::core::vector3df(2, 36.5, 0), true) {
+
+}
+
+NetworkCar::~NetworkCar() {
+
+}
+
+void NetworkCar::OnFrame() {
+
+}
