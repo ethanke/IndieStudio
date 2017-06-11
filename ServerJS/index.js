f968c6db72b596fd8ae1cdeace557c7ab3178cbc
root <root@ns325411.ip-37-187-8.eu>
2017-06-11 18:50:59 +0200

    server update, set car number

--- ServerJS/index.js
+++ ServerJS/index.js
@@ -15,31 +15,37 @@ mongoose.connect('mongodb://localhost:27017/IDC_db');
 
 io.on('connection', function(socket) {
     console.log("Client " + socket.id + " connected.");
-    
+
+    // ASK CURRENT CLIENT ID
     socket.emit('give your id',
 		{ message: 'Welcome!' }
 	       );
     
+    // REGISTER NEW USER / LOGIN KNOWN USERS
     socket.on('id client is', function(msg) {
 	var msg_obj = JSON.parse(msg);
 	Clients.find({'_id': msg_obj.id.toObjectId()}, function(err, clientList) {
-	    if(clientList.length != 0)
-	    {
-		
-	    }
-	    else
-	    {
-		var newClient = Clients();
+	    if(clientList.length == 0) {
+		var newClient = Clients({
+		    'socketID': socket.id
+		});
 		newClient.save(function(err, result) {
-		    if (result)
-		    {
+		    if (result) {
 			socket.emit('change id', {'new_id': result._id + ""});
 		    }
-		    else
-		    {
-			//FAIL SAVE BDD
-		    }
 		});
+	    } else {
+		Clients.update({"_id": msg_obj.id.toObjectId()}, {$set: {'socketID': socket.id}}, function(err, result) {});
+	    }
+	});
+    });
+
+    // CLIENT TELL US TO CHANGE HIS CAR_ID
+    socket.on('carNum changed', function(msg_str) {
+	var msg = JSON.parse(msg_str);
+	Clients.find({'_id': msg.id.toObjectId()}, function(err, clientList) {
+	    if(clientList.length != 0) {
+		Clients.update({"_id": msg.id.toObjectId()}, {$set: {'carNumber': msg.nbr}}, function(err, result) {});
 	    }
 	});
     });
